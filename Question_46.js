201edea7ef4933da94551812d01f872d65b44a25
SumitSharma1516 <sumitshar9340@gmail.com>
2024-12-27 23:48:26 -0800

    commit

--- Question_46.js
+++ Question_46.js
@@ -1,17 +1,20 @@
 // 46. Write a program to find total number of alphabets, digits or special character in a string.
 
-function app(str)
-{
-    let count=0;
-    for(check of str)
-    {
-        let num = parseInt(check)
-        console.log(num)
-        if(num === true)
-        {
+// The test() method tests for a match in a string.
+
+// If it finds a match, it returns true, otherwise it returns false.
+
+// /[a-zA-Z]/ A to Z Uppercase a to z lowercase
+
+function countAlphabets(str) {
+    let count = 0;
+    const characters = str.split('');
+    for (let i = 0; i < characters.length; i++) {
+        if (/[a-zA-Z]/.test(characters[i])) {
             count++;
         }
     }
-    console.log(count)
+    return count;
 }
-app("asd23")
+
+console.log(countAlphabets("Hello World123!")); // Output: 10
